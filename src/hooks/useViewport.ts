647e82c4a0c20c17aad742ffdd9cf4f6ec5fcba2
Tucker <trblackw@ncsu.edu>
2019-10-20 16:08:28 -0400

    Remove array deps causing infinite rerenders! oops

--- src/hooks/useViewport.ts
+++ src/hooks/useViewport.ts
@@ -35,7 +35,7 @@ export default (): [Dimension] => {
         window.removeEventListener("resize", deriveWindowDimensions)
       }
     }
-  }, [windowDimensions])
+  }, [])
 
   return [windowDimensions]
 }
