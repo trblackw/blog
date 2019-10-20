aff8d2d9039bf73712aa187597ef030babe7c528
Tucker <trblackw@ncsu.edu>
2019-10-20 16:50:07 -0400

    Remove unused global style sheet

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,5 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 
-import './src/styles/global.css'
 require("prismjs/themes/prism-okaidia.css")
 require("prismjs/plugins/line-numbers/prism-line-numbers.css")
